776419fdac5902b84b3e508da8361773b4a8b52e
zhabchi <ziad.habchi@gmail.com>
2014-10-16 13:33:29 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+include "app/config.php";
+include "app/detect.php";
+
+if ($page_name=='') {
+	include $browser_t.'/index.html';
+	}
+elseif ($page_name=='index.html') {
+	include $browser_t.'/index.html';
+	}
+elseif ($page_name=='about.html') {
+	include $browser_t.'/about.html';
+	}
+elseif ($page_name=='collections.html') {
+	include $browser_t.'/collections.html';
+	}
+elseif ($page_name=='blog.html') {
+	include $browser_t.'/blog.html';
+	}
+elseif ($page_name=='contact-post.html') {
+	include 'app/contact.php';
+	}
+else
+	{
+		include $browser_t.'/404.html';
+	}
+
+
+?>
