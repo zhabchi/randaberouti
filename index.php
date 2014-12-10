1e4ea3ad41ca627f7410ff35f81a168af36f7811
zhabchi <ziad.habchi@gmail.com>
2014-12-10 17:56:09 +0200

    fix

--- index.php
+++ index.php
@@ -12,11 +12,14 @@ elseif ($page_name=='index.html') {
 elseif ($page_name=='about.html') {
 	include $browser_t.'/about.html';
 	}
-elseif ($page_name=='collections.html') {
-	include $browser_t.'/collections.html';
+elseif ($page_name=='jewellery.html') {
+	include $browser_t.'/jewellery.html';
 	}
-elseif ($page_name=='blog.html') {
-	include $browser_t.'/blog.html';
+elseif ($page_name=='art.html') {
+	include $browser_t.'/art.html';
+	}
+elseif ($page_name=='poetry.html') {
+	include $browser_t.'/poetry.html';
 	}
 elseif ($page_name=='contact.html') {
 	include $browser_t.'/contact.html';
